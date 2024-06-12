4bfc01f2269ebdd1b9eb8a9fe74110b942af0425
kaisy <kaisymainam@gmail.com>
2024-06-12 09:58:40 +0300

    inserting values into table author

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,15 +1,15 @@
-
+--creating author table
 CREATE TABLE author (
     author_id SERIAL PRIMARY KEY,
     author_name TEXT,
     author_email TEXT
 );
-
+--creating genre table
 CREATE TABLE genre (
     genre_id SERIAL PRIMARY KEY,
     genre_name TEXT
 );
-
+--creating books table
 CREATE TABLE books (
     book_id SERIAL PRIMARY KEY,
     book_title TEXT,
@@ -17,3 +17,16 @@ CREATE TABLE books (
     author_id INTEGER REFERENCES author(author_id),
     genre_id INTEGER REFERENCES genre(genre_id)
 );
+
+
+--inserting data into table author
+INSERT INTO author (author_id, author_name, author_email) 
+VALUES 
+(1, 'J.K Rowling', 'rowlingsK@gmail.com'),
+(2, 'George R.R Martin', 'georgemartin@gmail.com'),
+(3, 'Stephen King', 'stephenking@gmail.com'),
+(4, 'Margaret Atwood', 'margaretatwood@gmail.com'),
+(5, 'Haruki Murakami', 'harukimurakami@gmail.com');
+
+
+--
