4fc52c3ccb1953bf3af5219e6735dfeb781b0e9d
kaisy <kaisymainam@gmail.com>
2024-06-12 10:56:12 +0300

    insrting values into books and genre

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -4,11 +4,7 @@ CREATE TABLE author (
     author_name TEXT,
     author_email TEXT
 );
---creating genre table
-CREATE TABLE genre (
-    genre_id SERIAL PRIMARY KEY,
-    genre_name TEXT
-);
+
 --creating books table
 CREATE TABLE books (
     book_id SERIAL PRIMARY KEY,
@@ -17,6 +13,13 @@ CREATE TABLE books (
     author_id INTEGER REFERENCES author(author_id),
     genre_id INTEGER REFERENCES genre(genre_id)
 );
+
+--creating genre table
+CREATE TABLE genre (
+    genre_id SERIAL PRIMARY KEY,
+    genre_name TEXT
+);
+
 
 
 --inserting data into table author
@@ -29,4 +32,33 @@ VALUES
 (5, 'Haruki Murakami', 'harukimurakami@gmail.com');
 
 
---
+--inserting data into table books
+INSERT INTO books (book_id, book_title, book_description, author_id) 
+VALUES 
+(1, 'Norwegian Wood', 'Melancholic tale of love and loss.', 5),
+(2, 'Harry Potter', 'Young wizards adventures at magical school.', 1),
+(3, 'The Handmaids Tale', 'Dystopian future where women are subjugated.', 4),
+(4, 'IT', 'Terrifying shape-shifting clown haunts small town.', 3),
+(5, 'A Song of Ice and Fire', 'Epic fantasy of power, betrayal, and dragons.', 2),
+(6, 'Oryx and Crake','Dystopian tale of genetic engineering gone wrong.', 4),
+(7, 'Kafka on the Shore', 'Surreal exploration of identity and human connection.', 5),
+(8, 'The Shining', 'Family trapped in haunted hotels dark past.', 3),
+(9, 'Game of Thrones', 'Epic fantasy of power struggles and royal intrigue.', 2),
+
+
+
+--insterting data into genre
+INSERT INTO genre (genre_id, genre_name, book_id ) 
+VALUES 
+(1, 'Dark Fantasy' 5),
+(2, 'Young Adult' 2),
+(3, 'Epic Fantasy' 5),
+(4, 'Romance' 1),
+(5, 'Speculative Fiction' 3),
+(6, 'Horror' 4),
+(7, 'Science Fiction' 3),
+(8, 'Literary Fiction'1 ),
+(9, 'Fantasy' 2),
+(10, 'Coming-of-Age'1 ),
+(11, 'Thriller' 4),
+(12, 'Dystopian' 3);
